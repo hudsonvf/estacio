2afdcb60455d53e85d7082930ccc76a3554696f6
Hudson Fonseca <101206748+hudsonvf@users.noreply.github.com>
2023-10-30 14:43:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,37 +15,3 @@ authorSearch.addEventListener('keyup', e => {
     })
 });
 
-//////////////////////////  CADASTRO  /////////////////////////////////////////////////////////////////////
-
-function sendEmail() {
-    Email.send({
-        SecureToken: "306d7c22-6db2-4942-b7c9-0b0eff5d4f6d",
-        To: "hudsonvsf@gmail.com",
-        From: "hudsonvdsf@gmail.com",
-        Subject: "Msg do site",
-        Body:
-            "Nome: " +
-            document.getElementById("cadastroNome").value +
-            "<br/> Email: " +
-            document.getElementById("cadastroEmail").value +
-            "<br/> Cep: " +
-            document.getElementById("cadastroCep").value +
-            "<br/> Estado: " +
-            document.getElementById("inputGroupSelect01").value +
-            "<br/> Cidade: " +
-            document.getElementById("cadastroCidade").value +
-            "<br/> Endereço: " +
-            document.getElementById("cadastroEndereco").value +
-            "<br/> Complemento: " +
-            document.getElementById("cadastroComplemento").value,
-    }).then(
-        message => {
-            if (message == "OK") {
-                swal("Mensagem enviada com sucesso!", "success");
-            } else {
-                swal("A mensagem não foi enviada, tente novamente...", "error");
-
-            }
-        }
-    );
-}
